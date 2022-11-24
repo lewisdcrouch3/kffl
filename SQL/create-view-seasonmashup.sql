4cffd5fa7c58fa27e5522d00dcc98a5155510a07
lewisdcrouch3 <lewiscrouch@gmail.com>
2022-11-23 17:15:28 -0700

    Create create-view-seasonmashup.sql

---
+++ SQL/create-view-seasonmashup.sql
@@ -0,0 +1,22 @@
+CREATE VIEW `seasonmashup` AS 
+select `rg`.`gameType` AS `gametype`
+    , year(`w`.`gameDate`) AS `Year`
+    , `w`.`year` AS `season`
+    , `w`.`week` AS `week`
+    , `w`.`gid` AS `gid`
+    , `t`.`name` AS `WinningTeam`
+    , `t`.`ownerID` AS `WinningOwnerID`
+    , `w`.`teamScore` AS `WinningTeamScore`
+    , `t2`.`name` AS `LosingTeam`
+    , `t2`.`ownerID` AS `LosingOwnerID`
+    , `l`.`teamScore` AS `LosingTeamScore`
+    , case 
+        when `w`.`division` = 0 then 'Yes' 
+        else 'No' end AS `DivisionGame`
+    , `w`.`location` AS `WinningTeamLocation` 
+from `gamedetail` `w` 
+left join `teams` `t` on `w`.`teamID` = `t`.`teamID` 
+left join `ref_gametype` `rg` on `w`.`gameTypeID` = `rg`.`gameTypeID` 
+left join `gamedetail` `l` on(`w`.`gid` = `l`.`gid` and `l`.`status` = 'L' 
+left join `teams` `t2` on `l`.`teamID` = `t2`.`teamID` 
+where `w`.`status` = 'W'
